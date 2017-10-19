5bc1bf560ac014459e87d47d81f10b6d1001173a
Cadichon <leo.labruyere@epitech.eu>
2017-10-19 11:35:52 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
-
-if [ $# -ne 1 ]
-then
-    echo "Please specify name.lastname"
-    exit 1
-fi
-
-
-sed -i "s/USER/$1/g" .emacs.d/epitech/std_comment.el
-
 mv .emacs ~
 rm -rf ~/.emacs.d/
 mv .emacs.d/ ~
